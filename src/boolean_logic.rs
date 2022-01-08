33d24fe5f5c82caf22bec1449f6e2fe1a51592f4
Joe Barnett <joebarnett876@gmail.com>
2022-01-08 14:30:57 +0000

    wip

--- src/boolean_logic.rs
+++ src/boolean_logic.rs
@@ -1,7 +1,64 @@
-pub struct NandGate;
+use std::cell::Cell;
 
-impl NandGate {
-    fn output(in_a: bool, in_b: bool) -> bool {
-        !(in_a && in_b)
+struct SourcePin {
+    value: Cell<bool>,
+}
+
+impl SourcePin {
+    fn new(value: bool) -> Self {
+        Self {
+            value: Cell::new(value),
+        }
+    }
+
+    fn set_value(&self, value: bool) {
+        self.value.set(value);
+    }
+}
+
+impl SourcePin {
+    fn get_value(&self) -> bool {
+        self.value.get()
+    }
+}
+
+struct NandOutPin<'a> {
+    in_a: &'a SourcePin,
+    in_b: &'a SourcePin,
+}
+
+impl NandOutPin<'_> {
+    fn get_value(&self) -> bool {
+        !(self.in_a.get_value() && self.in_b.get_value())
+    }
+}
+
+impl<'a> NandOutPin<'a> {
+    fn new(in_a: &'a SourcePin, in_b: &'a SourcePin) -> Self {
+        Self { in_a, in_b }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_const_pin() {
+        let pin = SourcePin::new(true);
+        assert_eq!(pin.get_value(), true);
+        pin.set_value(false);
+        assert_eq!(pin.get_value(), false);
+    }
+
+    #[test]
+    fn test_nand() {
+        let pin_a = SourcePin::new(true);
+        let pin_b = SourcePin::new(true);
+        let nand_out = NandOutPin::new(&pin_a, &pin_b);
+        assert_eq!(nand_out.get_value(), false);
+        pin_a.set_value(false);
+        pin_b.set_value(false);
+        assert_eq!(nand_out.get_value(), true);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 mod boolean_logic;
 
-use crate::boolean_logic::NandGate;
-
 fn main() {
     println!("Hello, world!");
 }
