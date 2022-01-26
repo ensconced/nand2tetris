5f134a14d2204016c7c984c809930a6aa03be786
Joe Barnett <joebarnett876@gmail.com>
2022-01-26 08:26:38 +0000

    wip

--- src/boolean_arithmetic.rs
+++ src/boolean_arithmetic.rs
@@ -287,13 +287,6 @@ fn test_is_non_zero() {
     }
 }
 
-// fn is_non_zero(a: [bool; 16]) -> bool {
-//     or(
-//         or8way([a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]]),
-//         or8way([a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15]]),
-//     )
-// }
-
 // struct ALUOutput {
 //     out: [bool; 16],
 //     zr: bool,
