8e1b328e63c55c03d9cb67f583a64faf841dbf92
Joe Barnett <joebarnett876@gmail.com>
2022-02-21 22:14:31 +0000

    almost done with cpu tests, just jumps remaining

--- fpga/test/cpu_tb.vhd
+++ fpga/test/cpu_tb.vhd
@@ -43,20 +43,21 @@ BEGIN
 
   stim : PROCESS
   BEGIN
+    -----------------------------------------------------------------------------------
     -- SETUP
     inM <= "0000000000000000";
     instruction <= "0000000000000000";
     reset <= '1';
     tick(clk);
     ASSERT (pc = "0000000000000000") REPORT "failed test at stage 1" SEVERITY failure;
-
+    -----------------------------------------------------------------------------------
     -- INCREMENTING PC
     reset <= '0';
     tick(clk);
     ASSERT (pc = "0000000000000001") REPORT "failed test at stage 2" SEVERITY failure;
     tick(clk);
     ASSERT (pc = "0000000000000010") REPORT "failed test at stage 3" SEVERITY failure;
-
+    -----------------------------------------------------------------------------------
     -- A + D
     -- load value "1" into register A
     instruction <= "0000000000000001";
@@ -71,27 +72,58 @@ BEGIN
     instruction <= "0000000000000010";
     tick(clk);
     ASSERT (addressM = "0000000000000010") REPORT "failed test at stage 7" SEVERITY failure;
-    -- add the values of registers A and D
-    instruction <= "1110000010000000";
+    -- add the values of registers A and D, and on next tick load result into register A
+    instruction <= "1110000010100000";
     WAIT FOR 5 ns;
     ASSERT (outM = "0000000000000011") REPORT "failed test at stage 8" SEVERITY failure;
-
+    tick(clk);
+    ASSERT (addressM = "0000000000000011") REPORT "failed test at stage 9" SEVERITY failure;
+    -----------------------------------------------------------------------------------
     -- D + M
     -- load value "3" into register A
     instruction <= "0000000000000011";
     tick(clk);
-    ASSERT (addressM = "0000000000000011") REPORT "failed test at stage 9" SEVERITY failure;
+    ASSERT (addressM = "0000000000000011") REPORT "failed test at stage 10" SEVERITY failure;
     -- load contents of register A into register D
     instruction <= "1110110000010000";
     tick(clk);
-    ASSERT (outM = "0000000000000011") REPORT "failed test at stage 10" SEVERITY failure;
+    ASSERT (outM = "0000000000000011") REPORT "failed test at stage 11" SEVERITY failure;
     -- set value "4" as inM
     inM <= "0000000000000100";
-    -- add the values of D and inM
-    instruction <= "1111000010000000";
+    -- add the values of D and inM, and load result into memory
+    instruction <= "1111000010001000";
     WAIT FOR 5 ns;
-    ASSERT (outM = "0000000000000111") REPORT "failed test at stage 11" SEVERITY failure;
-    -- TODO - d1d2d3
+    ASSERT (outM = "0000000000000111") REPORT "failed test at stage 12" SEVERITY failure;
+    ASSERT (writeM = '1') REPORT "failed test at stage 13" SEVERITY failure;
+    -----------------------------------------------------------------------------------
+    -- 10 x 10
+    -- load value "10" into register A
+    instruction <= "0000000000001010";
+    tick(clk);
+    -- load contents of register A into register D
+    instruction <= "1110110000010000";
+    tick(clk);
+    ASSERT (outM = "0000000000001010") REPORT "failed test at stage 13" SEVERITY failure;
+    -- repeat 9 x (compute A + D and load result into register D)
+    instruction <= "1110000010010000";
+    WAIT FOR 5 ns;
+    ASSERT (outM = "0000000000010100") REPORT "failed test at stage 14" SEVERITY failure;
+    tick(clk);
+    ASSERT (outM = "0000000000011110") REPORT "failed test at stage 15" SEVERITY failure;
+    tick(clk);
+    ASSERT (outM = "0000000000101000") REPORT "failed test at stage 15" SEVERITY failure;
+    tick(clk);
+    ASSERT (outM = "0000000000110010") REPORT "failed test at stage 15" SEVERITY failure;
+    tick(clk);
+    ASSERT (outM = "0000000000111100") REPORT "failed test at stage 15" SEVERITY failure;
+    tick(clk);
+    ASSERT (outM = "0000000001000110") REPORT "failed test at stage 15" SEVERITY failure;
+    tick(clk);
+    ASSERT (outM = "0000000001010000") REPORT "failed test at stage 15" SEVERITY failure;
+    tick(clk);
+    ASSERT (outM = "0000000001011010") REPORT "failed test at stage 15" SEVERITY failure;
+    tick(clk);
+    ASSERT (outM = "0000000001100100") REPORT "failed test at stage 15" SEVERITY failure;
     -- TODO - j1j2j3
     WAIT;
   END PROCESS;
