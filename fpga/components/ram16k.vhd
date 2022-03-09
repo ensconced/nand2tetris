5f1bb7fa58712988db2ffdd5dcfb57e1efef6a03
Joe Barnett <joebarnett876@gmail.com>
2022-03-09 22:11:41 +0000

    wip

--- fpga/components/ram16k.vhd
+++ fpga/components/ram16k.vhd
@@ -1,5 +1,7 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
+USE std.textio.ALL;
+USE ieee.numeric_std.ALL;
 
 ENTITY ram16k IS
   PORT (
@@ -11,35 +13,39 @@ ENTITY ram16k IS
 END ram16k;
 
 ARCHITECTURE structural OF ram16k IS
-  COMPONENT ram4k
-    PORT (
-      input : IN STD_ULOGIC_VECTOR(15 DOWNTO 0);
-      address : IN STD_ULOGIC_VECTOR(11 DOWNTO 0);
-      output : OUT STD_ULOGIC_VECTOR(15 DOWNTO 0);
-      load : IN STD_ULOGIC;
-      clock : IN STD_ULOGIC);
-  END COMPONENT;
-  COMPONENT dmux4way
-    PORT (
-      input : IN STD_ULOGIC;
-      sel : IN STD_ULOGIC_VECTOR(1 DOWNTO 0);
-      output : OUT STD_ULOGIC_VECTOR(3 DOWNTO 0));
-  END COMPONENT;
-  COMPONENT mux4way16
-    PORT (
-      input : IN STD_ULOGIC_VECTOR(63 DOWNTO 0);
-      sel : IN STD_ULOGIC_VECTOR(1 DOWNTO 0);
-      output : OUT STD_ULOGIC_VECTOR(15 DOWNTO 0)
-    );
-  END COMPONENT;
-  SIGNAL mux_input : STD_ULOGIC_VECTOR(63 DOWNTO 0);
-  SIGNAL dmux_output : STD_ULOGIC_VECTOR(3 DOWNTO 0);
+  CONSTANT C_INIT_FILE : STRING := "just use zeroes"; -- Specify name/location of RAM initialization file if using one (leave blank if not)
+  TYPE ram_type IS ARRAY (16383 DOWNTO 0) OF STD_ULOGIC_VECTOR (15 DOWNTO 0); -- 2D Array Declaration for RAM signal
+  FUNCTION initramfromfile (ramfilename : IN STRING) RETURN ram_type IS
+    FILE ramfile : text IS IN ramfilename;
+    VARIABLE ramfileline : line;
+    VARIABLE ram_name : ram_type;
+    VARIABLE bitvec : bit_vector(15 DOWNTO 0);
+  BEGIN
+    FOR i IN ram_type'RANGE LOOP
+      readline (ramfile, ramfileline);
+      read (ramfileline, bitvec);
+      ram_name(i) := to_stdlogicvector(bitvec);
+    END LOOP;
+    RETURN ram_name;
+  END FUNCTION;
+
+  FUNCTION init_from_file_or_zeroes(ramfile : STRING) RETURN ram_type IS
+  BEGIN
+    IF ramfile = "<Init File Name>" THEN
+      RETURN InitRamFromFile("<Init File Name>");
+    ELSE
+      RETURN (OTHERS => (OTHERS => '0'));
+    END IF;
+  END;
+  SIGNAL ram_name : ram_type := init_from_file_or_zeroes(C_INIT_FILE);
 BEGIN
-  mux : mux4way16 PORT MAP(input => mux_input, sel => address(13 DOWNTO 12), output => output);
-  dmux : dmux4way PORT MAP(input => load, sel => address(13 DOWNTO 12), output => dmux_output);
-  gen_ram4k :
-  FOR I IN 0 TO 3 GENERATE
-    ram4k_i : ram4k PORT MAP
-      (input => input, address => address(11 DOWNTO 0), output => mux_input((i * 16) + 15 DOWNTO (i * 16)), load => dmux_output(i), clock => clock);
-  END GENERATE gen_ram4k;
+  PROCESS (clock)
+  BEGIN
+    IF (clock 'event AND clock = '1') THEN
+      IF (load = '1') THEN
+        ram_name (to_integer(unsigned(address))) <= input;
+      END IF;
+    END IF;
+  END PROCESS;
+  output <= ram_name (to_integer(unsigned(address)));
 END structural;
